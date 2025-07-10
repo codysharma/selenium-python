92b0cd633d5aeddc88941f6715e6d338a6962a20
codysharma-ga <codysharmageneralassembly@gmail.com>
2025-07-10 14:41:59 -0700

    mutliple browsers tested

--- FirstPythonProject/tests/conftest.py
+++ FirstPythonProject/tests/conftest.py
@@ -5,23 +5,27 @@ from selenium.webdriver.common.by import By
 
 # pytest fixture is a function that runs before each test. If in the same folder as conftest.py, will auto import.
 
-@pytest.fixture()
+@pytest.fixture(params=
+                # ["chrome", "firefox", "edge"]
+                ["chrome"]
+                )
 def driver(request):
-    browser = request.config.getoption("--browser")
+    browser = request.param
+    # request.param is used to get the parameter from the fixture
     print(f"creating {browser} driver")
 
-    # my_driver = webdriver.Chrome()
-    # my_driver = webdriver.Firefox()
     if browser == "chrome":
         my_driver = webdriver.Chrome()
     elif browser == "firefox":
         my_driver = webdriver.Firefox()
+    elif browser == "edge":
+        my_driver = webdriver.Edge()
     else:
         raise TypeError(f"Expected chrome or firefox, but got {browser} instead.")
     
     my_driver.get("https://practicetestautomation.com/practice-test-login/")
     yield my_driver
-    # yield means this executes before the tests. Everything after runs after the test
+    # yield means this executes before the tests. Everything after, runs after the test
 
     print("closing driver")
     my_driver.quit()
